816b99eba2de7f228a01550f46d40c4865c5ca9e
George Semerikov <kdog2105@mail.ru>
2024-09-08 14:53:29 +0300

    add udp broadcast discover

--- main.js
+++ main.js
@@ -1,17 +1,27 @@
 const http = require("http");
 const request = require('request');
+var dgram = require('dgram');
+var server = dgram.createSocket("udp4");
+var url = require('url');
+
+var PORT = 41234;
+var BROADCAST_ADDR = "192.168.1.255";
+var discoverResult = []
 
 http.createServer(async function(req, res){
      
     res.setHeader("Content-Type", "application/json; charset=utf-8;");
-     
-    if(req.url === "/home" || req.url === "/"){
-        res.write("<h2>Home</h2>");
-        res.end();
+    
+    var queryData = url.parse(req.url, true);
+    console.dir(queryData)
+    if(queryData.pathname === "/api/getudp"){
+      res.write(JSON.stringify(await udp()));
+      res.end();
     }
-    else if(req.url == "/api/get"){
-        res.write(JSON.stringify(await getActiveAdresses()));
-        res.end();
+    else if(queryData.pathname === '/api/switchstate'){
+      var esp = discoverResult.find(el => el.Id == queryData.query.Id);
+      res.write(JSON.stringify(await doRequest('http://' + esp.address + '/RELAY=' + queryData.query.RELAY)));
+      res.end();
     }
     else{
         res.write("<h2>Not found</h2>");
@@ -19,27 +29,49 @@ http.createServer(async function(req, res){
     }
 }).listen(3000);
 
-async function getActiveAdresses(){
-    var arr = [];
-    for (var i = 2; i <= 100; i++){
-        try{
-            var a = JSON.parse(await doRequest('http://192.168.1.' + i + '/STATUS'));
-            a.addr = '192.168.1.' + i;
-            arr.push(a);
-        } catch(e){}
-    }
-    return arr;
+
+
+server.bind(PORT, function() {
+    server.setBroadcast(true);
+});
+
+server.on('error', (err) => {
+  console.error(`server error:\n${err.stack}`);
+  server.close();
+});
+
+server.on('message', (msg, rinfo) => {
+  //console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+  try{
+    var obj = JSON.parse(msg.toString())
+    obj.address = rinfo.address;
+    discoverResult.push(obj);
+  } catch (e){}
+});
+
+server.on('listening', () => {
+  const address = server.address();
+  console.log(`server listening ${address.address}:${address.port}`);
+});
+
+async function udp(){
+  var message = 'discover';
+  discoverResult = []
+  server.send(message, 0, message.length, PORT, BROADCAST_ADDR, () =>{});
+  return await new Promise(function (resolve) {
+    setTimeout(()=>{resolve(discoverResult);},200);
+    
+  });
 }
 
-
 function doRequest(url) {
-    return new Promise(function (resolve, reject) {
-      request.get(url, {timeout: 30}, function (error, res, body) {
-        if (!error && res.statusCode === 200) {
-          resolve(body);
-        } else {
-          reject(error);
-        }
-      });
+  return new Promise(function (resolve, reject) {
+    request.get(url, function (error, res, body) {
+      if (!error && res.statusCode === 200) {
+        resolve(body);
+      } else {
+        reject(error);
+      }
     });
+  });
 }
